c577fd750b6dc0f2cbcc9c79233258f98662af2f
Carl Page <crlpage7@gmail.com>
2019-09-06 19:09:40 -0500

    Initial dist subtree commit

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.carlwpage.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://carlpage.github.io
+# git push -f git@github.com:carlpage/carlpage.github.io.git master
+
+# if you are deploying to https://carlpage.github.io/personal-site
+git push -f git@github.com:carlpage/personal-site.git master:gh-pages
+
+cd -
