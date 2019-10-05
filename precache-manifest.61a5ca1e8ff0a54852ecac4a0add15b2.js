63d6930025751d2556cbb84584d7f268e43514e5
Carl Page <crlpage7@gmail.com>
2019-10-05 18:15:29 -0500

    Updates

---
+++ precache-manifest.61a5ca1e8ff0a54852ecac4a0add15b2.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "cff4dffde26e874a490a",
+    "url": "/personal-site/css/app.525e0c3f.css"
+  },
+  {
+    "revision": "cff4dffde26e874a490a",
+    "url": "/personal-site/js/app.e0166ab5.js"
+  },
+  {
+    "revision": "5c0180256d4ac39c3c7d",
+    "url": "/personal-site/js/chunk-vendors.439529c0.js"
+  },
+  {
+    "revision": "2b3e5f664b7d6ce8bf0cad49e4e61905",
+    "url": "/personal-site/img/apple-touch-icon.2b3e5f66.png"
+  },
+  {
+    "revision": "f3fd7bd9aa58875b977e49c9cc37322d",
+    "url": "/personal-site/img/kbh.f3fd7bd9.png"
+  },
+  {
+    "revision": "1db6b66011f6b800ef619461e8f87c11",
+    "url": "/personal-site/img/me-in-san-fransisco-desktop.1db6b660.jpg"
+  },
+  {
+    "revision": "0ad380d071c2d313866bba335d05ae84",
+    "url": "/personal-site/img/me-in-rome.0ad380d0.jpg"
+  },
+  {
+    "revision": "453157a447a781b4ca943938366feb3b",
+    "url": "/personal-site/img/metro-state.453157a4.jpg"
+  },
+  {
+    "revision": "cdca735e3dacbe11fed7fe14365a37cc",
+    "url": "/personal-site/img/android-chrome-192x192.cdca735e.png"
+  },
+  {
+    "revision": "e139ebc39363a5f75908a50aceb7291f",
+    "url": "/personal-site/img/poe.e139ebc3.png"
+  },
+  {
+    "revision": "58c5eb97e30c448693f6162960ecf044",
+    "url": "/personal-site/img/lifelinkiii.58c5eb97.png"
+  },
+  {
+    "revision": "94a7dfe4a2f4fc368976d1db61203a12",
+    "url": "/personal-site/img/mstile-150x150.94a7dfe4.png"
+  },
+  {
+    "revision": "002fbc2e8bb949861a764f0d61a9c304",
+    "url": "/personal-site/img/facial_recognition.002fbc2e.png"
+  },
+  {
+    "revision": "b953adc108981bffd5971d2bc1c6795a",
+    "url": "/personal-site/img/me-in-san-fransisco-mobile.b953adc1.jpg"
+  },
+  {
+    "revision": "0bdd764da42e7e765385554e2942253c",
+    "url": "/personal-site/img/comfy-chair.0bdd764d.svg"
+  },
+  {
+    "revision": "f36923fd1fb9e5338e31a7ea003ec4f0",
+    "url": "/personal-site/img/carl-page-no-bg.f36923fd.png"
+  },
+  {
+    "revision": "1775c01509c0a41918fc8e85bfc382aa",
+    "url": "/personal-site/img/mayo_clinic.1775c015.png"
+  },
+  {
+    "revision": "d6af873dc43f603a1a113965b62e21b0",
+    "url": "/personal-site/img/carl-in-rome.d6af873d.jpg"
+  },
+  {
+    "revision": "085ed6f51e573ae1efece93175c9f420",
+    "url": "/personal-site/img/nha.085ed6f5.png"
+  },
+  {
+    "revision": "cc33a0e522c5fbccd8c05bf0e59aa773",
+    "url": "/personal-site/img/android-chrome-512x512.cc33a0e5.png"
+  },
+  {
+    "revision": "705b4de34b62d81c12188dd64dd2132c",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "d6af873dc43f603a1a113965b62e21b0",
+    "url": "/personal-site/img/carl-in-rome.jpg"
+  },
+  {
+    "revision": "f36923fd1fb9e5338e31a7ea003ec4f0",
+    "url": "/personal-site/img/carl-page-no-bg.png"
+  },
+  {
+    "revision": "0bdd764da42e7e765385554e2942253c",
+    "url": "/personal-site/img/comfy-chair.svg"
+  },
+  {
+    "revision": "002fbc2e8bb949861a764f0d61a9c304",
+    "url": "/personal-site/img/facial_recognition.png"
+  },
+  {
+    "revision": "f3fd7bd9aa58875b977e49c9cc37322d",
+    "url": "/personal-site/img/kbh.png"
+  },
+  {
+    "revision": "58c5eb97e30c448693f6162960ecf044",
+    "url": "/personal-site/img/lifelinkiii.png"
+  },
+  {
+    "revision": "1775c01509c0a41918fc8e85bfc382aa",
+    "url": "/personal-site/img/mayo_clinic.png"
+  },
+  {
+    "revision": "0ad380d071c2d313866bba335d05ae84",
+    "url": "/personal-site/img/me-in-rome.jpg"
+  },
+  {
+    "revision": "1db6b66011f6b800ef619461e8f87c11",
+    "url": "/personal-site/img/me-in-san-fransisco-desktop.jpg"
+  },
+  {
+    "revision": "b953adc108981bffd5971d2bc1c6795a",
+    "url": "/personal-site/img/me-in-san-fransisco-mobile.jpg"
+  },
+  {
+    "revision": "453157a447a781b4ca943938366feb3b",
+    "url": "/personal-site/img/metro-state.jpg"
+  },
+  {
+    "revision": "8e692862b3ffbc75c6b4e69b2c0033de",
+    "url": "/personal-site/img/MJK.png"
+  },
+  {
+    "revision": "085ed6f51e573ae1efece93175c9f420",
+    "url": "/personal-site/img/nha.png"
+  },
+  {
+    "revision": "e139ebc39363a5f75908a50aceb7291f",
+    "url": "/personal-site/img/poe.png"
+  },
+  {
+    "revision": "bb51c7a48810cfcda1a1413156626506",
+    "url": "/personal-site/img/prime.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/personal-site/robots.txt"
+  }
+];
